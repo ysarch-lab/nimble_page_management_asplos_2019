d7a92b4371341cf26a3ace0ec0be1209da6bfe31
Michal Hocko <mhocko@suse.com>
2018-01-03 09:25:54 +0100

    mm, migrate: remove reason argument from new_page_t
    No allocation callback is using this argument anymore. new_page_node
    used to use this parameter to convey node_id resp. migration error
    up to move_pages code (do_move_page_to_node_array). The error status
    never made it into the final status field and we have a better way
    to communicate node id to the status field now. All other allocation
    callbacks simply ignored the argument so we can drop it finally.
    
    Reviewed-by: Zi Yan <zi.yan@cs.rutgers.edu>
    Signed-off-by: Michal Hocko <mhocko@suse.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1139,10 +1139,9 @@ static ICE_noinline int unmap_and_move(new_page_t get_new_page,
 				   enum migrate_reason reason)
 {
 	int rc = MIGRATEPAGE_SUCCESS;
-	int *result = NULL;
 	struct page *newpage;
 
-	newpage = get_new_page(page, private, &result);
+	newpage = get_new_page(page, private);
 	if (!newpage)
 		return -ENOMEM;
 
@@ -1233,12 +1232,6 @@ put_new:
 			put_page(newpage);
 	}
 
-	if (result) {
-		if (rc)
-			*result = rc;
-		else
-			*result = page_to_nid(newpage);
-	}
 	return rc;
 }
 
@@ -1266,7 +1259,6 @@ static int unmap_and_move_huge_page(new_page_t get_new_page,
 				enum migrate_mode mode, int reason)
 {
 	int rc = -EAGAIN;
-	int *result = NULL;
 	int page_was_mapped = 0;
 	struct page *new_hpage;
 	struct anon_vma *anon_vma = NULL;
@@ -1283,7 +1275,7 @@ static int unmap_and_move_huge_page(new_page_t get_new_page,
 		return -ENOSYS;
 	}
 
-	new_hpage = get_new_page(hpage, private, &result);
+	new_hpage = get_new_page(hpage, private);
 	if (!new_hpage)
 		return -ENOMEM;
 
@@ -1348,12 +1340,6 @@ out:
 	else
 		putback_active_hugepage(new_hpage);
 
-	if (result) {
-		if (rc)
-			*result = rc;
-		else
-			*result = page_to_nid(new_hpage);
-	}
 	return rc;
 }
 
@@ -1801,8 +1787,7 @@ static bool migrate_balanced_pgdat(struct pglist_data *pgdat,
 }
 
 static struct page *alloc_misplaced_dst_page(struct page *page,
-					   unsigned long data,
-					   int **result)
+					   unsigned long data)
 {
 	int nid = (int) data;
 	struct page *newpage;
